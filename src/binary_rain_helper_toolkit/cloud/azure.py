b3ff90e192a07c7fb0b4144db35b8ab1d37174cd
Marcel T.O <marcel@turobin-ort.at>
2024-08-07 17:16:45 +0200

    Adjusted dataframe functions to support optional dictionary settings

--- src/binary_rain_helper_toolkit/cloud/azure.py
+++ src/binary_rain_helper_toolkit/cloud/azure.py
@@ -1,13 +1,22 @@
 import json
-import azure.functions as func
+import pandas as pd
+import logging
 
-def return_http_response(
-    message: str,
-    status_code: int
-) -> func.HttpResponse:
+import azure.functions as func
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient
+
+from data_processing.dataframe import (
+    create_dataframe,
+    from_dataframe_to_type,
+    FileFormat,
+)
+
+
+def return_http_response(message: str, status_code: int) -> func.HttpResponse:
     """
     Format an HTTP response.
-    
+
     ### Parameters:
     ----------
     message : str
@@ -26,10 +35,165 @@ def return_http_response(
         status = "NOK"
 
     return func.HttpResponse(
-        json.dumps({
-            "response": message,
-            "status": status
-        }),
+        json.dumps({"response": message, "status": status}),
         status_code=status_code,
         mimetype="application/json",
     )
+
+
+def read_blob_data(
+    blob_account: str,
+    container_name: str,
+    blob_name: str,
+    file_format: FileFormat = FileFormat.PARQUET,
+) -> pd.DataFrame:
+    """
+    Read data from a blob storage account.
+
+    ### Parameters:
+    ----------
+    blob_account : str
+        The name of the blob account.
+    container_name : str
+        The name of the container.
+    blob_name : str
+        The name of the blob.
+    file_format : FileFormat, optional
+        The format of the file to be loaded. Default is FileFormat.PARQUET.
+
+    ### Returns:
+    ----------
+    df : pd.DataFrame
+        The data read from the blob.
+    exception : ValueError
+        If an error occurs while trying to download the blob data.
+    """
+    # Initialize the dataframe
+    df = pd.DataFrame()
+
+    # validate the input parameters
+    if not blob_account or not container_name or not blob_name:
+        error_msg = "Error: Missing required input parameters."
+        logging.error(error_msg)
+        return ValueError(error_msg)
+
+    try:
+        # Create blob service client
+        blob_service_client = BlobServiceClient(
+            blob_account,
+            credential=DefaultAzureCredential(),
+        )
+
+        # Create container service client
+        blob_client = blob_service_client.get_blob_client(
+            container_name,
+            blob_name,
+        )
+
+        chunks_data = blob_client.download_blob()
+        chunk_list = []
+
+        # Download the data in chunks.
+        # This is useful for large files, since Files over 35MB can cause issues.
+        for chunk in chunks_data.chunks():
+            chunk_list.append(chunk)
+
+        # Combine the chunks into a single byte array
+        byte_array = b"".join(chunk_list)
+
+        # Load data into a pandas dataframe
+        df = create_dataframe(byte_array, file_format)
+    except Exception as e:  # pylint: disable=broad-except
+        error_msg = "Error while trying to download the blob data. Exception: %s", e
+        logging.exception(error_msg)
+        return return_http_response(error_msg, 500)
+    return df
+
+
+def upload_blob_data(
+    blob_account: str,
+    container_name: str,
+    blob_name: str,
+    df: pd.DataFrame,
+    file_format: FileFormat = FileFormat.PARQUET,
+    file_format_options: dict = None,
+) -> pd.DataFrame:
+    """
+    Upload data from a blob storage account.
+
+    ### Parameters:
+    ----------
+    blob_account : str
+        The name of the blob account.
+    container_name : str
+        The name of the container.
+    blob_name : str
+        The name of the blob.
+    file_format : FileFormat, optional
+        The format of the file to be loaded. Default is FileFormat.PARQUET.
+    file_format_options : dict, optional
+        The format options for the file. Default is None.
+
+    ### Returns:
+    ----------
+    df : pd.DataFrame
+        The data read from the blob.
+    exception : ValueError
+        If an error occurs while trying to download the blob data.
+
+    ### Example:
+    ----------
+    ```python
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+    file_format_options = {
+        'compression': 'snappy',
+        'use_deprecated_int96_timestamps': True
+    }
+    upload_blob_data(
+        blob_account='blob_account',
+        container_name='container_name',
+        blob_name='blob_name',
+        df=df,
+        file_format=FileFormat.PARQUET,
+        file_format_options=file_format_options
+    )
+    ```
+    """
+    # Initialize the dataframe
+    df = pd.DataFrame()
+
+    # validate the input parameters
+    if not blob_account or not container_name or not blob_name:
+        error_msg = "Error: Missing required input parameters."
+        logging.error(error_msg)
+        return ValueError(error_msg)
+
+    try:
+        # Create blob service client
+        blob_service_client = BlobServiceClient(
+            blob_account,
+            credential=DefaultAzureCredential(),
+        )
+
+        # Create container service client
+        blob_client = blob_service_client.get_blob_client(
+            container_name,
+            blob_name,
+        )
+
+        # Upload the fixed dataframe to the blob
+        blob = from_dataframe_to_type(
+            df, file_format, file_format_options=file_format_options
+        )
+        blob_client.upload_blob(
+            blob,
+            overwrite=True,
+            blob_type="BlockBlob",
+            length=len(blob),
+            connection_timeout=800,
+        )
+    except Exception as e:  # pylint: disable=broad-except
+        error_msg = "Error while trying to download the blob data. Exception: %s", e
+        logging.exception(error_msg)
+        return return_http_response(error_msg, 500)
+    return df
--- src/binary_rain_helper_toolkit/data_processing/dataframe.py
+++ src/binary_rain_helper_toolkit/data_processing/dataframe.py
@@ -1,12 +1,19 @@
 import io
 import logging
+from enum import Enum
 import pandas as pd
+
+class FileFormat(Enum):
+    PARQUET = 1
+    CSV = 2
+    DICT = 3
+    JSON = 4
 
 
 def create_dataframe(
     file_contents: bytes | dict,
-    file_format: str,
-    seperator: str = None,
+    file_format: FileFormat,
+    file_format_options: dict | None = None,
 ):
     """
     Create a dataframe from the file contents.
@@ -15,10 +22,10 @@ def create_dataframe(
     ----------
     file_contents : bytes | dict
         The contents of the file to be loaded.
-    file_format : str
-        The format of the file to be loaded. Currently supported: "csv" and "dict".
-    seperator : str, optional
-        The seperator used in the csv file. Default is None.
+    file_format : FileFormat
+        The format of the file to be loaded. Currently supported: "csv" and "dict", "parquet".
+    file_format_options : dict | None
+        The options for the file format. Default is None.
 
     ### Returns:
     ----------
@@ -28,10 +35,27 @@ def create_dataframe(
         If an error occurs during dataframe creation
     """
     try:
-        if file_format == "csv":
-            dataframe = pd.read_csv(io.BytesIO(file_contents), sep=seperator)
-        elif file_format == "dict":
-            dataframe = pd.DataFrame.from_dict(file_contents)
+        if file_format == FileFormat.CSV:
+            if file_format_options is None:
+                dataframe = pd.read_csv(io.BytesIO(file_contents))
+            else:
+                dataframe = pd.read_csv(io.BytesIO(file_contents), **file_format_options)
+            dataframe = pd.read_csv(io.BytesIO(file_contents), )
+        elif file_format == FileFormat.DICT:
+            if file_format_options is None:
+                dataframe = pd.DataFrame.from_dict(file_contents)
+            else:
+                dataframe = pd.DataFrame.from_dict(file_contents, **file_format_options)
+        elif file_format == FileFormat.PARQUET:
+            if file_format_options is None:
+                dataframe = pd.read_parquet(io.BytesIO(file_contents), engine="pyarrow")
+            else:
+                dataframe = pd.read_parquet(io.BytesIO(file_contents), engine="pyarrow", **file_format_options)
+        elif file_format == FileFormat.JSON:
+            if file_format_options is None:
+                dataframe = pd.read_json(io.BytesIO(file_contents))
+            else:
+                dataframe = pd.read_json(io.BytesIO(file_contents), **file_format_options)
         else:
             # other laod formats will be added later/when needed, e.g. load from json
             raise TypeError(
@@ -42,6 +66,64 @@ def create_dataframe(
         logging.exception(error_msg)
         raise ValueError(error_msg) from exc
     return dataframe
+
+def from_dataframe_to_type(
+    dataframe: pd.DataFrame,
+    file_format: FileFormat,
+    file_format_options: dict | None = None,
+) -> bytes:
+    """
+    Converts the dataframe to a specific file format.
+
+    ### Parameters:
+    ----------
+    file_contents : bytes | dict
+        The contents of the file to be loaded.
+    file_format : FileFormat
+        The format of the file to be loaded.
+    file_format_options : dict | None
+        The options for the file format. Default is None.
+
+    ### Returns:
+    ----------
+    content : bytes
+        The file contents
+    exception : ValueError
+        If an error occurs during dataframe conversion
+    """
+    try:
+        if file_format == FileFormat.CSV:
+            if file_format_options is None:
+                content = dataframe.to_csv(index=False)
+            else:
+                content = dataframe.to_csv(index=False, **file_format_options)
+        elif file_format == FileFormat.DICT:
+            if file_format_options is None:
+                content = dataframe.to_dict(orient="records")
+            else:
+                content = dataframe.to_dict(**file_format_options)
+        elif file_format == FileFormat.PARQUET:
+            if file_format_options is None:
+                content = dataframe.to_parquet(engine="pyarrow")
+            else:
+                content = dataframe.to_parquet(
+                    engine="pyarrow", 
+                    **file_format_options
+                )
+        elif file_format == FileFormat.JSON:
+            if file_format_options is None:
+                content = dataframe.to_json()
+            else:
+                content = dataframe.to_json(**file_format_options)
+        else:
+            raise TypeError(
+                f"Error converting dataframe. Unknown file format: {file_format}"
+            )
+    except Exception as exc:
+        error_msg = f"Error converting dataframe. Exception: {exc}"
+        logging.exception(error_msg)
+        raise ValueError(error_msg) from exc
+    return content
 
 
 def merge_dataframes(
